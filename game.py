0e1a6c70c2ded442d50c89134ceeba3955fa443a
ichizokichinosuke <nuuuukemw@gmail.com>
2020-05-07 13:01:41 +0900

    to functional

--- game.py
+++ game.py
@@ -45,46 +45,53 @@ def disp_result(dealer, player):
 
     print(f"Winner is {who_win}!!")
 
-while(True):
-    print("Game Start...")
-    dealer = 0
-    player = 0
-    try_first = True
+def run()
     while(True):
-        if dealer < 17:
-            dealer += new_card(who="d")
-        player += new_card(who="p")
-        if try_first:
-            try_first = False
-            continue
+        print("Game Start...")
+        dealer = 0
+        player = 0
+        try_first = True
+        while(True):
+            if dealer < 17:
+                dealer += new_card(who="d")
+            player += new_card(who="p")
+            if try_first:
+                try_first = False
+                continue
 
-        print(f"Dealer's total number is {dealer}.")
-        print(f"Player's total number is {player}.")
+            print(f"Dealer's total number is {dealer}.")
+            print(f"Player's total number is {player}.")
 
-        is_continue = check_num(dealer, player)
+            is_continue = check_num(dealer, player)
 
-        if not is_continue:
-            break
+            if not is_continue:
+                break
 
-        choice = choice_action()
-
-        while(not(choice == "s" or choice == "h")):
             choice = choice_action()
 
-        if choice == "s":
-            while(dealer < 17):
-                dealer += new_card(who="d")
-                print(f"Dealer's total number is {dealer}.")
+            while(not(choice == "s" or choice == "h")):
+                choice = choice_action()
 
-            disp_result(dealer, player)
+            if choice == "s":
+                while(dealer < 17):
+                    dealer += new_card(who="d")
+                    print(f"Dealer's total number is {dealer}.")
+
+                disp_result(dealer, player)
+                break
+            elif choice == "h":
+                continue
+
+        print("Play again?")
+        print("Please enter Yes: y or No: n.")
+        game_again = input()
+        if game_again == "y" or game_again == "Y":
+        continue
+        else:
             break
-        elif choice == "h":
-            continue
 
-    print("Play again?")
-    print("Please enter Yes: y or No: n.")
-    game_again = input()
-    if game_again == "y" or game_again == "Y":
-       continue
-    else:a
-        break
+def main():
+    run()
+
+if __name__ == "__main__":
+    main()
